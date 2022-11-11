403da3d6305c546f7e8f254f026fe8995eccc531
TejasNanavati <117970380+TejasNanavati@users.noreply.github.com>
2022-11-11 12:17:20 +0530

    Add files via upload

---
+++ CartrackAssigmentUITests/CartrackAssigmentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CartrackAssigmentUITestsLaunchTests.swift
+//  CartrackAssigmentUITests
+//
+//  Created by Tejas Nanavati on 08/11/22.
+//
+
+import XCTest
+
+class CartrackAssigmentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
